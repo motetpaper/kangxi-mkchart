649f9d978d468189258ab383877f2f3718d8ab85
motetpaper <71auras_aegis@icloud.com>
2025-01-27 21:50:01 -0800

    Update motetkangxi.py
    * added yaml preferences file
    * nudged the hanzi text upwards

--- motetkangxi.py
+++ motetkangxi.py
@@ -5,6 +5,16 @@
 # lic   : MIT
 
 from PIL import Image, ImageDraw, ImageFont
+import yaml
+
+with open('chart.yaml') as f:
+    prefs = yaml.safe_load(f)
+
+print(prefs)
+
+chartHanziFont = prefs['chart']['fonts']['hanzi']
+chartPinyinFont = prefs['chart']['fonts']['pinyin']
+chartNumberFont = prefs['chart']['fonts']['number']
 
 ## colors
 wht = (255,255,255)
@@ -14,20 +24,20 @@ blk = (0,0,0)
 bxdims = (125,275)
 
 ## font area
-nmfont=ImageFont.truetype(f'./fonts/GentiumPlus-Bold.ttf',size=48)
-hzfont=ImageFont.truetype(f'./fonts/ShipporiMincho-Bold.ttf',size=100)
-pyfont=ImageFont.truetype(f'./fonts/GentiumPlus-Bold.ttf',size=36)
+nmfont=ImageFont.truetype(f'./fonts/{chartNumberFont}',size=48)
+hzfont=ImageFont.truetype(f'./fonts/{chartHanziFont}',size=100)
+pyfont=ImageFont.truetype(f'./fonts/{chartPinyinFont}',size=36)
 
 ## header fonts
-strksfont=ImageFont.truetype(f'./fonts/GentiumPlus-Bold.ttf',size=80)
-unitsfont=ImageFont.truetype(f'./fonts/ShipporiMincho-Bold.ttf',size=100)
+strksfont=ImageFont.truetype(f'./fonts/{chartPinyinFont}',size=80)
+unitsfont=ImageFont.truetype(f'./fonts/{chartHanziFont}',size=100)
 
 def test_box(num,hz,py):
 	with Image.new('RGB',bxdims,wht) as img:
 		d = ImageDraw.Draw(img)
 		d.rectangle((0,0)+bxdims, fill=wht, outline=blk, width=4)
 		d.text((10,8),str(num).rjust(3),fill=blk,font=nmfont)
-		d.text((10,80),hz,fill=blk,font=hzfont)
+		d.text((10,60),hz,fill=blk,font=hzfont)
 		d.text((10,200),py.center(7),fill=blk,font=pyfont)
 		return img
 
